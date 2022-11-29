2e208fa8ca6e6df95501238c793f5102a7f2141c
carlos gizbert <carlos.gizbert@fcamara.com.br>
2022-11-29 01:19:34 -0300

    fix deploy ec2 htpps

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { Order, OrderStatus } from '../types/Order'
 
 export const api = axios.create({
-  baseURL: 'https://18.231.110.184:3001',
+  baseURL: 'http://18.231.110.184:3001',
 })
 
 export async function getOrders() {
