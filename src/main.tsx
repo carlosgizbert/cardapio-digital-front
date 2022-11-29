b088c9b15aa069008b3e8bd08c758570c6bea300
carlos gizbert <carlos.gizbert@fcamara.com.br>
2022-11-29 01:16:27 -0300

    fix deploy ec2

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
-  <App />
+  <>
+    <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+    <App />
+  </>
   // </React.StrictMode>
 )
