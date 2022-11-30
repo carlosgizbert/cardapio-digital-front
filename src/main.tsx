16e16af0e3d288b8fab449df18548f312aed4090
carlos gizbert <carlos.gizbert@fcamara.com.br>
2022-11-29 23:43:29 -0300

    fix ip websocket

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <React.StrictMode>
-  <>
-    <head>
-      <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-    </head>
+  <React.StrictMode>
     <App />
-  </>
-  // </React.StrictMode>
+  </React.StrictMode>
 )
