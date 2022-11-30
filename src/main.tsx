ee14b13a908a1bae2c61d808aafad5f589511263
carlos gizbert <carlos.gizbert@fcamara.com.br>
2022-11-29 23:54:35 -0300

    fix httpEquiv

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <head>
-      <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-    </head>
     <App />
   </React.StrictMode>
 )
